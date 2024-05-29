f094d1a1eb0b9ec149e70547b90b988eec98b677
ongkyoktafian1 <ongky.johansyah@ajaib.co.id>
2024-05-29 15:21:54 +0700

    add auto approve.

--- auto_approve.groovy
+++ auto_approve.groovy
@@ -4,14 +4,3 @@ ScriptApproval scriptApproval = ScriptApproval.get()
 scriptApproval.pendingScripts.each {
     scriptApproval.approveScript(it.hash)
 }
-
-/* Script to clear script approval 
-$JENKINS_HOME/init.groovy.d/disable-script-security.groovy:
-*/
-
-import javaposse.jobdsl.plugin.GlobalJobDslSecurityConfiguration
-import jenkins.model.GlobalConfiguration
-
-// disable Job DSL script approval
-GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).useScriptSecurity = false
-GlobalConfiguration.all().get(GlobalJobDslSecurityConfiguration.class).save()
